9711df616a88dd3f311107e17ccd5971f074b4ea
Pernelle Mike <69342409+PernelleM92@users.noreply.github.com>
2021-03-04 14:17:57 +0100

    suite

---
+++ bibliotheque.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 26, 2021 at 01:19 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bibliotheque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `abonne`
+--
+
+CREATE TABLE `abonne` (
+  `id_abonne` int(3) NOT NULL,
+  `prenom` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `abonne`
+--
+
+INSERT INTO `abonne` (`id_abonne`, `prenom`) VALUES
+(1, 'Guillaume'),
+(2, 'Benoit'),
+(3, 'Chloe'),
+(4, 'Laura');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emprunt`
+--
+
+CREATE TABLE `emprunt` (
+  `id_emprunt` int(3) NOT NULL,
+  `id_livre` int(3) DEFAULT NULL,
+  `id_abonne` int(3) DEFAULT NULL,
+  `date_sortie` date NOT NULL,
+  `date_rendu` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `emprunt`
+--
+
+INSERT INTO `emprunt` (`id_emprunt`, `id_livre`, `id_abonne`, `date_sortie`, `date_rendu`) VALUES
+(1, 100, 1, '2014-12-17', '2014-12-18'),
+(2, 101, 2, '2014-12-18', '2014-12-20'),
+(3, 100, 3, '2014-12-19', '2014-12-22'),
+(4, 103, 4, '2014-12-19', '2014-12-22'),
+(5, 104, 1, '2014-12-19', '2014-12-28'),
+(6, 105, 2, '2015-03-20', '2015-03-26'),
+(7, 105, 3, '2015-06-13', NULL),
+(8, 100, 2, '2015-06-15', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `livre`
+--
+
+CREATE TABLE `livre` (
+  `id_livre` int(3) NOT NULL,
+  `auteur` varchar(30) NOT NULL,
+  `titre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `livre`
+--
+
+INSERT INTO `livre` (`id_livre`, `auteur`, `titre`) VALUES
+(100, 'GUY DE MAUPASSANT', 'Une vie'),
+(101, 'GUY DE MAUPASSANT', 'Bel-Ami '),
+(102, 'HONORE DE BALZAC', 'Le père Goriot'),
+(103, 'ALPHONSE DAUDET', 'Le Petit chose'),
+(104, 'ALEXANDRE DUMAS', 'La Reine Margot'),
+(105, 'ALEXANDRE DUMAS', 'Les Trois Mousquetaires');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `abonne`
+--
+ALTER TABLE `abonne`
+  ADD PRIMARY KEY (`id_abonne`);
+
+--
+-- Indexes for table `emprunt`
+--
+ALTER TABLE `emprunt`
+  ADD PRIMARY KEY (`id_emprunt`),
+  ADD KEY `id_livre` (`id_livre`),
+  ADD KEY `id_abonne` (`id_abonne`);
+
+--
+-- Indexes for table `livre`
+--
+ALTER TABLE `livre`
+  ADD PRIMARY KEY (`id_livre`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `abonne`
+--
+ALTER TABLE `abonne`
+  MODIFY `id_abonne` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `emprunt`
+--
+ALTER TABLE `emprunt`
+  MODIFY `id_emprunt` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `livre`
+--
+ALTER TABLE `livre`
+  MODIFY `id_livre` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `emprunt`
+--
+ALTER TABLE `emprunt`
+  ADD CONSTRAINT `emprunt_ibfk_1` FOREIGN KEY (`id_abonne`) REFERENCES `abonne` (`id_abonne`),
+  ADD CONSTRAINT `emprunt_ibfk_2` FOREIGN KEY (`id_livre`) REFERENCES `livre` (`id_livre`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
